43c7a262bdaf0a81e978f42d1687dde888bd5e47
adb <asieldbenitez@gmail.com>
2024-11-02 06:36:07 +0100

    add support for multiple scoreboards (#2)
    add support for multiple scoreboards
    
    also improve the init() API to allow complete control over
    info-message generation and ability to refresh/update custom scoreboard

---
+++ example/vite.config.js
@@ -0,0 +1,5 @@
+import { webxdcViteConfig } from "webxdc-vite-plugins";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(webxdcViteConfig());
--- src/webxdc-scores.js
+++ src/webxdc-scores.js
@@ -1,16 +1,14 @@
 import jsSHA from "jssha";
 
 export const highscores = (() => {
-  let players = [],
-    _appName = "",
-    container = undefined;
-  const selfID = new jsSHA("SHA-512", "TEXT", { encoding: "UTF8" })
-    .update(window.webxdc.selfAddr)
-    .getHash("HEX");
-  const maxserialKey = "webxdc-scores.max_serial",
-    playersKey = "webxdc-scores.players";
+  let scoreboards = {},
+    _getAnnouncement = (name, score) => `${name} scored ${score}`,
+    _compareScores = (score1, score2) => score1 - score2,
+    _onHighscoresChanged = () => {};
+  const maxserialKey = "_webxdc-scores_.max_serial",
+    scoreboardsKey = "_webxdc-scores_.scoreboards";
 
-  function h(tag, attributes, ...children) {
+  const h = (tag, attributes, ...children) => {
     const element = document.createElement(tag);
     if (attributes) {
       Object.entries(attributes).forEach((entry) => {
@@ -19,106 +17,134 @@ export const highscores = (() => {
     }
     element.append(...children);
     return element;
-  }
+  };
 
-  function getScore(id) {
+  const getScore = (id, scoreboard) => {
+    const players = scoreboards[scoreboard] || {};
     return players[id] ? players[id].score : 0;
-  }
-
-  function getHighScores() {
-    const scores = Object.keys(players)
-      .map((id) => {
-        return {
-          current: id === selfID,
-          ...players[id],
-        };
-      })
-      .sort((a, b) => b.score - a.score);
-
-    for (let i = 0; i < scores.length; i++) {
-      scores[i].pos = i + 1;
-    }
-
-    return scores;
-  }
-
-  function updateScoreboard() {
-    if (!container) return;
-
-    let table = getHighScores();
-    let div = h("div");
-    for (let i = 0; i < table.length; i++) {
-      const player = table[i];
-      const pos = h("span", { class: "row-pos" }, player.pos);
-      pos.innerHTML += ".&nbsp;&nbsp;";
-      div.appendChild(
-        h(
-          "div",
-          { class: "score-row" + (player.current ? " you" : "") },
-          pos,
-          h("span", { class: "row-name" }, player.name),
-          h("span", { class: "row-score" }, player.score)
-        )
-      );
-    }
-    container.innerHTML = div.innerHTML;
-  }
+  };
 
   return {
-    selfID: selfID,
-    init: (appName, scoreboard) => {
-      _appName = appName;
-      if (scoreboard) {
-        container = document.getElementById(scoreboard);
+    selfID: new jsSHA("SHA-512", "TEXT", { encoding: "UTF8" })
+      .update(window.webxdc.selfAddr)
+      .getHash("HEX"),
+    init: function ({
+      getAnnouncement,
+      compareScores,
+      onHighscoresChanged,
+    } = {}) {
+      if (getAnnouncement) {
+        _getAnnouncement = getAnnouncement;
       }
-      players = JSON.parse(localStorage.getItem(playersKey) || "{}");
-      updateScoreboard();
-      return window.webxdc.setUpdateListener((update) => {
-        const player = update.payload;
-        if (player.force || player.score > getScore(player.id)) {
-          players[player.id] = { name: player.name, score: player.score };
-        }
-        if (update.serial === update.max_serial) {
-          localStorage.setItem(playersKey, JSON.stringify(players));
-          localStorage.setItem(maxserialKey, update.max_serial);
-          updateScoreboard();
-        }
-      }, parseInt(localStorage.getItem(maxserialKey) || 0));
+
+      if (compareScores) {
+        _compareScores = compareScores;
+      }
+
+      if (onHighscoresChanged) {
+        _onHighscoresChanged = onHighscoresChanged;
+      }
+
+      scoreboards = JSON.parse(localStorage.getItem(scoreboardsKey) || "{}");
+      for (const scoreboard of Object.keys(scoreboards)) {
+        _onHighscoresChanged(scoreboard);
+      }
+
+      return window.webxdc.setUpdateListener(
+        (update) => {
+          const player = update.payload;
+          const board = player.scoreboard;
+          if (
+            player.force ||
+            _compareScores(player.score, getScore(player.id, board), board) > 0
+          ) {
+            if (scoreboards[board] === undefined) {
+              scoreboards[board] = {};
+            }
+            scoreboards[board][player.id] = {
+              name: player.name,
+              score: player.score,
+            };
+          }
+          if (update.serial === update.max_serial) {
+            localStorage.setItem(scoreboardsKey, JSON.stringify(scoreboards));
+            localStorage.setItem(maxserialKey, update.max_serial);
+            _onHighscoresChanged(board);
+          }
+        },
+        parseInt(localStorage.getItem(maxserialKey) || 0),
+      );
     },
 
-    getScore: () => {
-      return getScore(selfID);
+    getScore: function (scoreboard) {
+      return getScore(this.selfID, scoreboard);
     },
 
-    setScore: function (score, force = false) {
-      const old_score = this.getScore();
-      if (score > old_score || force) {
+    setScore: function (score, force = false, scoreboard = undefined) {
+      const oldScore = this.getScore(scoreboard);
+      const selfID = this.selfID;
+      if (force || _compareScores(score, oldScore, scoreboard) > 0) {
+        if (scoreboards[scoreboard] === undefined) {
+          scoreboards[scoreboard] = {};
+        }
         const name = window.webxdc.selfName;
-        players[selfID] = { name: name, score: score };
-        let info = name + " scored " + score;
-        if (_appName) {
-          info += " in " + _appName;
-        }
+        scoreboards[scoreboard][selfID] = { name: name, score: score };
+        const info = _getAnnouncement(name, score, scoreboard);
         window.webxdc.sendUpdate(
           {
             payload: {
               id: selfID,
-              name: name,
-              score: score,
-              force: force,
+              name,
+              score,
+              force,
+              scoreboard,
             },
-            info: info,
+            info,
           },
-          info
+          "",
         );
       } else {
-        console.log(
-          "[webxdc-score] Ignoring score: " + score + " <= " + old_score
-        );
+        console.log(`[webxdc-score] Ignoring score: ${score} <= ${oldScore}`);
       }
     },
 
-    getHighScores: getHighScores,
+    getHighScores: function (scoreboard) {
+      const players = scoreboards[scoreboard] || {};
+      const selfID = this.selfID;
+      const scores = Object.keys(players)
+        .map((id) => {
+          return {
+            current: id === selfID,
+            ...players[id],
+          };
+        })
+        .sort((a, b) => _compareScores(b.score, a.score, scoreboard));
+
+      for (let i = 0; i < scores.length; i++) {
+        scores[i].pos = i + 1;
+      }
+
+      return scores;
+    },
+
+    renderScoreboard: function (scoreboard) {
+      let table = this.getHighScores(scoreboard);
+      let div = h("div");
+      for (const player of table) {
+        const pos = h("span", { class: "row-pos" }, player.pos);
+        pos.innerHTML += ".&nbsp;&nbsp;";
+        div.appendChild(
+          h(
+            "div",
+            { class: "score-row" + (player.current ? " you" : "") },
+            pos,
+            h("span", { class: "row-name" }, player.name),
+            h("span", { class: "row-score" }, player.score),
+          ),
+        );
+      }
+      return div;
+    },
   };
 })();
 
